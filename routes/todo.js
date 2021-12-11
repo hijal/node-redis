d37663d31864d5ddca4c56601ded048faad6c09a
imh1j4l <hijal@shuttlebd.com>
2021-12-11 20:37:36 +0600

    uncomment all route

--- routes/todo.js
+++ routes/todo.js
@@ -3,9 +3,9 @@ const todoController = require('../controllers/todoController');
 const router = express.Router();
 
 router.get('/', todoController.getAll);
-// router.post('/', todoController.create);
-// router.get('/:id', todoController.findById);
-// router.put('/:id', todoController.update);
-// router.delete('/:id', todoController.remove);
+router.post('/', todoController.create);
+router.get('/:id', todoController.findById);
+router.put('/:id', todoController.update);
+router.delete('/:id', todoController.remove);
 
 module.exports = router;
